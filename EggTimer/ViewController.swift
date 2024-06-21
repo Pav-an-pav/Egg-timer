e7d72769e79cc0117d5cff0019511c9c8c5788ed
Pav-an-pav <pav-an-pav@yandex.com>
2024-06-21 16:22:10 +0500

    final

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -13,9 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var titleEgg: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     
-    let eggTime = ["Soft": 3,
-                   "Medium": 4,
-                   "Hard": 7]
+    let eggTime = ["Soft": 300,
+                   "Medium": 420,
+                   "Hard": 720]
     // 300, 420, 720
     var timer = Timer()
     var totalTime = 0
---
+++ README.md
@@ -0,0 +1,8 @@
+![App Banner](Documentation/AppBanner.png)
+
+# EggTimer
+
+A beautiful egg timer app to boil your eggs to perfection depending on how you prefer your eggs.
+
+
+>The project was created using tutorials from Udemy course by Angela Yu, The App Brewery's Complete App Development Bootcamp
